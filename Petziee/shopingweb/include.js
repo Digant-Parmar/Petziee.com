7b7ac491eaac6e41d0ab7d642ad14ea7a14068d5
digant <digantparmar17@gmail.com>
2021-10-07 22:47:20 +0530

    Need some necessary changes

--- Petziee/shopingweb/include.js
+++ Petziee/shopingweb/include.js
@@ -1,8 +1,9 @@
 $(function() {
     var includes = $('[data-include]')
     $.each(includes, function() {
-        var file = 'http://localhost:5005/' +
-            // 'https://petziee-dev.web.app/' +
+        var file =
+            // 'http://localhost:5005/' +
+            'https://petziee-dev.web.app/' +
             $(this).data('include') + '.html'
         $(this).load(file)
     })
